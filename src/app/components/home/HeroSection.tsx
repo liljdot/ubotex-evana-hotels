acf99083add88d2d972a524ebfbb3a0af552c7a9
liljdot <jehuoki@gmail.com>
2025-05-03 22:14:06 +0100

    Add HeroSection component with background image and introductory text

---
+++ src/app/components/home/HeroSection.tsx
@@ -0,0 +1,30 @@
+import heroBG from "@/app/assets/hero-bg.png"
+import { openSans } from "@/app/layout";
+import ActionButton from "../ActionButton";
+import { whatsappContact } from "@/app/data";
+
+const HeroSection: React.FC = () => {
+
+    return (
+        <>
+            <section style={{ backgroundImage: `linear-gradient(to bottom, #000000EE, #00000000), url(${heroBG.src})` }} className="bg-cover bg-center pt-17 md:pt-24 lg:pt-40 xl:pt-55 pb-12.5 md:pb-16 lg:pb-24 xl:pb-37">
+                <div className="flex flex-col gap-4 md:gap-6 lg:gap-8 xl:gap-10">
+                    <div className="flex flex-col gap-5 xl:gap-15 text-neutral text-center">
+                        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-7xl xl:text-[158px] xl:font-medium px-5 xl:px-18">Welcome
+                            <br />
+                            to Evana Hotels</h1>
+                        <p className={"text-sm xl-text-base px-16 xl:px-31 " + openSans.className}>
+                            Premium spaces, curated experiences, and personalized service, discover the new standard in hospitality.                        </p>
+                    </div>
+
+                    <div className="flex justify-center items-center">
+                        <ActionButton to={whatsappContact} className="text-primary">Contact Us</ActionButton>
+                    </div>
+                </div>
+
+            </section>
+        </>
+    )
+}
+
+export default HeroSection;
