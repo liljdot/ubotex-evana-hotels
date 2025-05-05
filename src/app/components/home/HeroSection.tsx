ce082ac393a4b14ee854091d6b0d0fb423c0ef14
liljdot <jehuoki@gmail.com>
2025-05-05 17:51:16 +0100

    Update HeroSection styles: adjust padding and text sizes for improved layout

--- src/app/components/home/HeroSection.tsx
+++ src/app/components/home/HeroSection.tsx
@@ -7,13 +7,13 @@ const HeroSection: React.FC = () => {
 
     return (
         <>
-            <section style={{ backgroundImage: `linear-gradient(to bottom, #000000EE, #00000000), url(${heroBG.src})` }} className="bg-cover bg-center pt-17 md:pt-24 lg:pt-40 xl:pt-55 pb-12.5 md:pb-16 lg:pb-24 xl:pb-37">
+            <section style={{ backgroundImage: `linear-gradient(to bottom, #000000EE, #00000000), url(${heroBG.src})` }} className="bg-cover bg-center pt-30 lg:pt-40 xl:pt-55 pb-12.5 md:pb-16 lg:pb-24 xl:pb-37">
                 <div className="flex flex-col gap-4 md:gap-6 lg:gap-8 xl:gap-10">
                     <div className="flex flex-col gap-5 xl:gap-15 text-neutral text-center">
-                        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-7xl xl:text-[158px] xl:font-medium px-5 xl:px-18">Welcome
+                        <h1 className="text-5xl lg:text-7xl xl:text-[158px] font-bold px-5 xl:px-18">Welcome
                             <br />
                             to Evana Hotels</h1>
-                        <p className={"text-sm xl-text-base px-16 xl:px-31 " + openSans.className}>
+                        <p className={"text-base px-12 xl:px-31 " + openSans.className}>
                             Premium spaces, curated experiences, and personalized service, discover the new standard in hospitality.                        </p>
                     </div>
 
