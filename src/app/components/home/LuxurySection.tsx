9031dabb34cb0666885106caef92ecd48b2fcea3
liljdot <jehuoki@gmail.com>
2025-05-19 21:19:28 +0100

    Refactor LuxurySection to use ContentSection for improved structure

--- src/app/components/home/LuxurySection.tsx
+++ src/app/components/home/LuxurySection.tsx
@@ -46,7 +46,7 @@ const LuxurySection: React.FC = () => {
                             ">
                 <div className="flex flex-col gap-1 md:gap-2 lg:gap-3 xl:gap-4">
                     <h1 className="text-primary text-2xl md:text-3xl lg:text-5xl xl:text-7xl font-bold">Luxury Redefined</h1>
-                    <p className={"text-base-content text-sm md:text-base text-justify " + openSans.className}>At Evana Hotels, luxury is not just seen, it's felt. Every detail, from our interiors to our dining, speaks of timeless elegance and bespoke hospitality.</p>
+                    <ContentSection.Text className={openSans.className}>At Evana Hotels, luxury is not just seen, it's felt. Every detail, from our interiors to our dining, speaks of timeless elegance and bespoke hospitality.</ContentSection.Text>
                 </div>
 
                 <div
